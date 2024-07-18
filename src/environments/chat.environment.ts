374e3a56563e6beac8e7ac4fd08261dc3a17a61a
Sana720 <saahmed311@gmail.com>
2024-07-18 16:08:56 +0530

    Chat Working with Firebase

---
+++ src/environments/chat.environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCkE75H93pOaKr3edTCEMyMrXvl_XhAqxw",
+    authDomain: "lgn-ionic.firebaseapp.com",
+    projectId: "lgn-ionic",
+    storageBucket: "lgn-ionic.appspot.com",
+    messagingSenderId: "851627686040",
+    appId: "1:851627686040:web:0ceda129290659a0c178ed",
+    measurementId: "G-YPX5EKWV09"
+  }
+
+
+};
